14697fbe4a4f179b9e4f4dd5c227c089e522a9e9
“jsantamariar” <santamaria.web.dev@gmail.com>
2024-02-13 20:25:29 -0500

    feat: adding pinia state managment and creating notes store

--- src/views/NotesView.vue
+++ src/views/NotesView.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="text-center mt-10">
     <h1 class="text-4xl my-2 cursor-default text-stone-700 font-bold dark:text-white">Notes</h1>
-    <div class="inline-flex mx-3 m-5" v-for="item in mockData" :key="item.id">
-      <CardNotes :data="item" />
+    <div class="inline-flex mx-3 m-5" v-for="note in notes" :key="note.id">
+      <CardNotes :data="note" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { useNotesStore } from '@/store/notes'
 import CardNotes from '@/components/Layout/CardNotes.vue'
-import { mockData } from '@/utils/mockData'
+
+const notesStore = useNotesStore()
+const { notes } = storeToRefs(notesStore)
 </script>
 
 <style scoped></style>
