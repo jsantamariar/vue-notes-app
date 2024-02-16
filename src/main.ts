b1615a21e88adaf98deb067050e170962d0ec09e
“jsantamariar” <santamaria.web.dev@gmail.com>
2024-02-15 20:54:50 -0500

    fix: placing pinia at the end to get pinia in devtools

--- src/main.ts
+++ src/main.ts
@@ -7,4 +7,4 @@ import App from './App.vue'
 import router from './router'
 import i18n from './locale/i18n'
 
-createApp(App).use(router).use(createPinia()).use(CKEditorPlugin).use(i18n()).mount('#app')
+createApp(App).use(router).use(CKEditorPlugin).use(i18n()).use(createPinia()).mount('#app')
