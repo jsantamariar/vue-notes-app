79bcbb2a04494279cc42d1b261696aefb095ef76
“jsantamariar” <santamaria.web.dev@gmail.com>
2024-02-16 16:18:59 -0500

    feat: adding EmptyNotes component to show when there are no notes

---
+++ src/components/Layout/EmptyNotes.vue
@@ -0,0 +1,47 @@
+<template>
+  <section class="bg-white dark:bg-gray-900">
+    <div class="flex flex-col items-center max-w-sm mx-auto text-center">
+      <p class="p-3 text-sm font-medium text-blue-500 rounded-full bg-blue-50 dark:bg-gray-800">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="2"
+          stroke="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
+          />
+        </svg>
+      </p>
+      <h1 class="mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl">
+        There are no notes yet
+      </h1>
+      <p class="mt-4 text-gray-500 dark:text-gray-400">
+        {{ authStore.isLogged ? 'Create a new note' : 'Log in and create a new note' }}
+      </p>
+
+      <div class="flex items-center w-full mt-6 gap-x-3 shrink-0 sm:w-auto">
+        <button
+          @click="
+            authStore.isLogged ? $router.push({ name: 'Admin' }) : $router.push({ name: 'Login' })
+          "
+          class="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600"
+        >
+          {{ authStore.isLogged ? 'Create' : 'Login' }}
+        </button>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { useAuthStore } from '@/store/auth'
+
+const authStore = useAuthStore()
+</script>
+
+<style scoped></style>
