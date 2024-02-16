d37dd83a61eecc49624b0b12c265f336114bfcf9
“jsantamariar” <santamaria.web.dev@gmail.com>
2024-02-16 16:19:14 -0500

    feat: displaying note content in NoteDetailsView

--- src/views/NoteDetailsView.vue
+++ src/views/NoteDetailsView.vue
@@ -1,13 +1,25 @@
 <template>
   <div class="text-center mt-32">
-    <div class="inline-flex mx-3 m-5 text-stone-700">
-      {{ params.id }}
+    <div class="flex justify-center flex-col mx-3 m-5 text-stone-700">
+      <div>
+        {{ noteContent?.title }}
+      </div>
+      {{ noteContent?.created }}
+      <div></div>
+      <div>
+        {{ noteContent?.createdBy }}
+      </div>
+      <div v-html="noteContent?.description"></div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { useNotesStore } from '@/store/notes'
 import { useRoute } from 'vue-router'
 
+const { getNoteContentById } = useNotesStore()
+
 const { params } = useRoute()
+const noteContent = getNoteContentById(params.id as string)
 </script>
--- src/views/NotesView.vue
+++ src/views/NotesView.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="text-center mt-10">
-    <h1 class="text-4xl my-2 cursor-default text-stone-700 font-bold dark:text-white">Notes</h1>
+    <h1
+      v-if="notes.length > 0"
+      class="text-4xl my-2 cursor-default text-stone-700 font-bold dark:text-white"
+    >
+      Notes
+    </h1>
     <div class="inline-flex mx-3 m-5" v-for="note in notes" :key="note.id">
       <CardNotes :note="note" />
     </div>
-    <div class="inline-flex mx-3 m-5">
+    <div class="container flex justify-center items-center min-h-screen px-6 py-12 mx-auto">
       <LoadingSpinner v-if="store.isLoadingNotes" />
+      <EmptyNotes v-if="notes.length < 1 && !store.isLoadingNotes" />
     </div>
-    <div v-if="notes.length < 1 && !store.isLoadingNotes">There are not notes yet</div>
   </div>
 </template>
 
@@ -17,6 +22,7 @@ import { useNotesStore } from '@/store/notes'
 import { storeToRefs } from 'pinia'
 import CardNotes from '@/components/Layout/CardNotes.vue'
 import LoadingSpinner from '@/components/Layout/LoadingSpinner.vue'
+import EmptyNotes from '@/components/Layout/EmptyNotes.vue'
 
 const store = useNotesStore()
 const { notes } = storeToRefs(store)
